f24fd134a54cff2655cc034de612187cec7298be
J. Brad Byrd <brad@jbradbyrd.net>
2021-11-29 22:29:30 -0500

    Keep matching tokens

--- src/compiler.rs
+++ src/compiler.rs
@@ -3,7 +3,14 @@ use super::scanner::{Scanner};
 pub fn compile(source: &str) {
     let mut scanner = Scanner::new(source);
     let mut line = -1;
-    loop {
-        
+    while let Some(token) = scanner.scan_token() {
+        if token.line != line {
+            print!("{:>4}", token.line);
+            line = token.line;
+        } else {
+            print!("   | ");
+        }
+
+        println!("{:>2} '{}'", token.token_type as i32, token.string);
     }
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,24 +1,101 @@
-use std::str::Chars;
+use std::{iter::{Enumerate, Peekable}, str::{CharIndices, Chars}};
 
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    
+    // Literals.
+    Identifier,
+    String,
+    Number,
+    
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    For,
+    Fun,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+  
+    Error,
+    Eof,
+}
+  
 pub struct Token<'src> {
-    string: &'src str,
-    line: i32,
+    pub token_type: TokenType,
+    pub string: &'src str,
+    pub line: i32,
 }
 
 pub struct Scanner<'src> {
-    chars: Chars<'src>,
+    source: &'src str,
+    chars: Peekable<Enumerate<Chars<'src>>>,
+    start: usize,
     line: i32,
 }
 
 impl<'src> Scanner<'src> {
     pub fn new(source: &'src str) -> Self {
         Scanner {
-            chars: source.chars(),
+            source,
+            chars: source.chars().enumerate().peekable(),
+            start: 0,
             line: 1,
          }
     }
 
-    pub fn scan_token() -> Token<'src> {
+    pub fn scan_token(&mut self) -> Option<Token<'src>> {
+        if let Some((i, ch)) = self.chars.next() {
+            self.start = i;
+            match ch {
+                '(' => self.make_token(TokenType::LeftParen, i),
+
+                _ => None,
+            }
+
+        } else {
+            None
+        }
         
+        /*
+        */
+    }
+
+    fn make_token(&self, token_type: TokenType, length: usize) -> Option<Token<'src>> {
+        Some(Token {
+            token_type,
+            string: &self.source[self.start .. length],
+            line: self.line,
+        })
     }
 }
